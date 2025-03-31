b7498c3cffdf4319c8fc05cb8899f4de2b088fcd
Jesus Guzman <amorales130@alumnos.uaq.mx>
2025-03-30 23:03:18 -0600

    Nuevos elementos

--- 4-cameras/src/script.js
+++ 4-cameras/src/script.js
@@ -8,9 +8,24 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Sizes
 const sizes = {
-    width: 800,
-    height: 600
+    width: window.innerWidth,
+    height: window.innerHeight
 }
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
 
 // Scene
 const scene = new THREE.Scene()
